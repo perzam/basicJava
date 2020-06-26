8ca048c42562d5762d89d095f381abeb602401b6
Javier Perez <perzam@gmail.com>
2020-06-25 21:09:16 -0500

    cambio pequeño

--- src/bHolaMundo/Hello.java
+++ src/bHolaMundo/Hello.java
@@ -3,6 +3,6 @@ package bHolaMundo.fHelloWorld;
 public class Hello {
 
 	public static void main(String args[]) {
-		System.out.println("hello, world\n");  //funcion para imprimir en consola
+		System.out.println("hello, world perzam\n");  //funcion para imprimir en consola
 	}
 }
